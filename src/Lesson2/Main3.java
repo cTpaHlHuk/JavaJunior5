efe1f2b1d9e3ad8dc7a0b019eb14c9592447b96d
cTpaHHuk <izbranov.nikita@mail.ru>
2022-11-28 20:42:33 +0300

    1

---
+++ src/Lesson2/Main3.java
@@ -0,0 +1,9 @@
+package Lesson2;
+
+public class Main3 {
+    public static void main(String[] args) {
+        for (int i = 150; i > 100; i--) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Lesson2/Main4.java
@@ -0,0 +1,10 @@
+package Lesson2;
+
+public class Main4 {
+    public static void main(String[] args) {
+        for (int i = 10; i < 50; i++) {
+            if ((i + 2) % 3 == 0)
+                System.out.println(i);
+        }
+    }
+}
